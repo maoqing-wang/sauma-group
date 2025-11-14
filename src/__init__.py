11783e0892a40bfb2fb2ae3261addd1790d7e7d6
VivianGuo0 <158122524+VivianGuo0@users.noreply.github.com>
2025-11-13 23:03:08 -0500

    create csv

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,9 @@
 from src.io_utils import read_parquet_dir, save_dataframe
 from src.preprocessing import rolling_window, expanding_window
-from src.metrics import compute_EI, compute_rolling_metrics
+from src.metrics import calc_metrics, compute_EI_time, compute_EI_trader
 from src.visualization import plot_pnl_EI
 
 __all__ = ['read_parquet_dir', 'save_dataframe', 
            'rolling_window', 'expanding_window', 
-           'compute_EI', 'compute_rolling_metrics', 
+           'calc_metrics', 'compute_EI_trader', 'compute_EI_time',
            'plot_pnl_EI']
